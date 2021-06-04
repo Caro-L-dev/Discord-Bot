704765b059af535bb80d5109f41514eac1ea9aec
Caro-L-dev <liardcar@gmail.com>
2021-06-04 19:44:08 +0200

    :hammer: Add player YouTube on !play command

--- commands/play.js
+++ commands/play.js
@@ -1,17 +1,25 @@
 module.exports = {
     name: 'play',
-    description: 'Lecture des sons demandés.',
-    async execute(message) {
+    description: 'Lecture audio.',
+    async execute(message, arguments) {
         if (!message.guild) return;
 
         if (message.member.voice.channel) {
+            const ytdl = require('ytdl-core');
             const connection = await message.member.voice.channel.join();
-            const dispatcher = connection.play('./audio.mp3', {
+            const dispatcher = connection.play(ytdl(arguments[0]), {
                 volume: 0.5,
             });
             dispatcher.on('start', () => {
-              message.client.user.setActivity('YouTube', { type: 'LISTENING' })  
+              message.client.user.setActivity('de la musique', { type: 'LISTENING' })  
             })
+
+            dispatcher.on('error', () => {
+                message.reply(`Je n'ai pas réussi à lire cette vidéo !`);
+                dispatcher.destroy();
+                message.member.voice.channel.leave();
+              })
+
             dispatcher.on('finish', () => {
                 dispatcher.destroy();
                 message.member.voice.channel.leave();
