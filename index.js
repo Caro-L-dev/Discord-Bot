7b977878535b59c34c4e54d6488d0b7b905c3f66
Caro-L-dev <liardcar@gmail.com>
2021-06-04 15:59:00 +0200

    :hammer: Add !server command + config file

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-require('dotenv').config();
-
+/*require('dotenv').config();*/
 const Discord = require('discord.js');
+
+const { prefix, token } = require('./config.json');
 
 const client = new Discord.Client();
 
@@ -10,10 +11,15 @@ client.once('ready', () => {
     console.log('Ready!');
 });
 
+
+/*Guild represents a server on Discord */
 client.on('message', (message) => {
-    if(message.content === '!ping') {
+    if(message.content === `${prefix}ping`) {
         message.channel.send('Pong.');
+    }
+    else if (message.content === `${prefix}server`) {
+        message.channel.send(`Nom du serveur : ${message.guild.name}\n Nombre d'utilisateurs : ${message.guild.memberCount} `);
     }
 });
 
-client.login(process.env.DISCORD_TOKEN);
+client.login(token);
