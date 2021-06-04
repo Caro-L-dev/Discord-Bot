cfe47ad81ed6cf5e90cad53d3245d9725a05dafe
Caro-L-dev <liardcar@gmail.com>
2021-06-04 17:22:36 +0200

    :heavy_plus_sign: Add fs dependency

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-/*require('dotenv').config();*/
+const fs = require('fs');
 const Discord = require('discord.js');
 
 const { prefix, token } = require('./config.json');
@@ -9,35 +9,6 @@ const client = new Discord.Client();
 /* Method that fetch an event*/
 client.once('ready', () => {
     console.log('Ready!');
-});
-
-
-
-client.on('message', (message) => {
-
-    if (!message.content.startsWith(prefix) || message.author.bot) return;
-
-    const arguments = message.content.slice(prefix.length).split(/ +/);
-    const command = arguments.shift().toLocaleLowerCase();
-
-    if(command === `ping`) {
-        message.channel.send('Pong.');
-    }
-    else if (command === `server`) {
-        /*Guild represents a server on Discord */
-        message.channel.send(`Nom du serveur : ${message.guild.name}\n Nombre d'utilisateurs : ${message.guild.memberCount}`);
-    }
-    else if (command === `avatar`) {
-       if (!message.mentions.users.size) {
-           return message.channel.send(`Ton avatar est : ${message.author.displayAvatarURL({format: 'jpg' })}`);
-       }
-
-       const avatarList = message.mentions.users.map(user => {
-           return `L'avatar de ${user.username} est ${user.displayAvatarURL({format: 'jpg' })}`;
-       });
-
-       message.channel.send(avatarList);
-    }
 });
 
 client.login(token);
