2c7d62c02d7212ec837affb95c3a0279224a51b9
Caro-L-dev <liardcar@gmail.com>
2021-06-04 18:34:07 +0200

    :hammer: Add !play command

--- commands/play.js
+++ commands/play.js
@@ -1,7 +1,24 @@
 module.exports = {
     name: 'play',
     description: 'Lecture des sons demandés.',
-    execute(message) {
-        
+    async execute(message) {
+        if (!message.guild) return;
+
+        if (message.member.voice.channel) {
+            const connection = await message.member.voice.channel.join();
+            const dispatcher = connection.play('./audio.mp3', {
+                volume: 0.5,
+            });
+            dispatcher.on('start', () => {
+              message.client.user.setActivity('YouTube', { type: 'LISTENING' })  
+            })
+            dispatcher.on('finish', () => {
+                dispatcher.destroy();
+                message.member.voice.channel.leave();
+              })
+        } 
+        else {
+            message.reply(`Tu dois d'abord rejoindre un salon audio !`);
+        }
     }
 };
